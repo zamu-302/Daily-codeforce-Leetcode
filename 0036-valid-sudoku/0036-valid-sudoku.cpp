92fbec3291ac7ff4b6f46e880f15b2b2caf91215
Samuel Admaw Anley <samueladmaw23@gmail.com>
2026-09-14 16:19:26 +0300

    0036-VALID-SUDOKU Stats: Time: 0 ms (100%), Space: 21.5 MB (92.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        std::unordered_set<char> rows[9];
-        std::unordered_set<char> cols[9];
-        std::unordered_set<char> boxes[9];
+        std::array<std::array<bool,9>,9> rows{}; 
+        std::array<std::array<bool,9>,9> cols{};  
+        std::array<std::array<bool,9>,9> boxes{};         
+        
 
+        int boardIdx;    
         for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
+            for(int j=0; j<9;j++){
                 if(board[i][j]=='.'){
                     continue;
                 }
-                auto val{board[i][j]};
-                int box_index=(i/3)*3+(j/3);
-
-                if(rows[i].contains(val) || cols[j].contains(val) || boxes[box_index].contains(val)){
+                boardIdx=((i/3)*3)+(j/3);
+                int num=board[i][j]-'1';
+                if(rows[i][num] || cols[j][num] || boxes[boardIdx][num]){
                     return false;
                 }
-                rows[i].insert(val);
-                cols[j].insert(val);
-                boxes[box_index].insert(val);
+                rows[i][num]=true;
+                cols[j][num]=true;
+                boxes[boardIdx][num]=true;
+                
+
+
             }
         }
         return true;
+
     }
 };
