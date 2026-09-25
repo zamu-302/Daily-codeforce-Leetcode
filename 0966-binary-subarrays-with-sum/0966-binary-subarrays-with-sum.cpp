910087993e15683cc761a1ab466c265fd12311fd
Samuel Admaw Anley <samueladmaw23@gmail.com>
2026-09-25 12:55:37 +0300

    0966-BINARY-SUBARRAYS-WITH-SUM Stats: Time: 0 ms (100%), Space: 32.6 MB (82.78%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        std::unordered_map<int,int> seen;
-        int ans=0;
+    constexpr int summ(std::vector<int>& nums,int goal) noexcept{
+        int left=0;
+        int right=0;
+        int target=0;
         int curr=0;
-        for(const auto& n:nums){
-            curr+=n;
-            if(curr==goal){
-                ans++;
+        while(right<nums.size()){
+            curr+=nums[right];
+            while(goal<curr && left<=right){
+                curr-=nums[left];
+                left++;
             }
-            if(seen.find(curr-goal)!=seen.end()){
-                ans+=seen[curr-goal];
+            if(curr<=goal){
+             target+=(right-left+1);
             }
-            seen[curr]++;
+            right++;
+        
         }
-        return ans;
+        return target;
+    }
+
+    constexpr int numSubarraysWithSum(vector<int>& nums, int goal) noexcept {
+       return summ(nums,goal)-summ(nums,goal-1);
+       
     }
 };
